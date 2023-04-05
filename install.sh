913c66f2e44ed9e8c3abc498f2c9bad740b47e71
root <mohannad.alhanahnah@gmail.com>
2023-04-05 11:50:31 +0000

    minor fixes

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ echo "Installing dependencies."
 sudo apt-get install -y build-essential gpg wget lsb-release software-properties-common
 
 # CMake
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo  tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 
-echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ bionic main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ bionic main' | sudo  tee /etc/apt/sources.list.d/kitware.list >/dev/null
 sudo apt-get update
 # CMake and Ninja
 sudo apt-get install -y cmake ninja-build
